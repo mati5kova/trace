2fa0da36dab6433550fb16ccaac23723e1e2087c
Matevz <matevz5.kovacic@gmail.com>
2026-07-10 02:13:55 +0200

    utils for duration

--- include/trace/utils.hpp
+++ include/trace/utils.hpp
@@ -5,10 +5,18 @@
 #ifndef TRACE_UTILS_H
 #define TRACE_UTILS_H
 
+#include <chrono>
 #include <string>
 
 namespace trace::utils{
     void replace_escape_characters_with_printable(std::string &str);
+
+    std::chrono::microseconds get_duration_us(std::chrono::time_point<std::chrono::system_clock> t1,
+                                              std::chrono::time_point<std::chrono::system_clock> t2);
+
+    std::chrono::nanoseconds get_duration_ns(std::chrono::time_point<std::chrono::system_clock> t1,
+                                             std::chrono::time_point<std::chrono::system_clock> t2);
+
 }
 
 #endif //TRACE_UTILS_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -5,9 +5,20 @@
 #include "trace/utils.hpp"
 
 #include <regex>
+#include <chrono>
 
 void trace::utils::replace_escape_characters_with_printable(std::string &str) {
     str = std::regex_replace(str, std::regex("\r"), "\\r");
     str = std::regex_replace(str, std::regex("\n"), "\\n");
     str = std::regex_replace(str, std::regex("\t"), "\\t");
+}
+
+std::chrono::microseconds trace::utils::get_duration_us(const std::chrono::time_point<std::chrono::system_clock> t1,
+                                                        const std::chrono::time_point<std::chrono::system_clock> t2) {
+    return std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+}
+
+std::chrono::nanoseconds trace::utils::get_duration_ns(const std::chrono::time_point<std::chrono::system_clock> t1,
+                                                        const std::chrono::time_point<std::chrono::system_clock> t2) {
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1);
 }
